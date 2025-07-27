025dc03fa5a65ff2281c0bc3ae36dc971e37762a
Bruno Souza <souza.brunha@gmail.com>
2025-07-27 17:39:51 -0300

    test: endpoint para leitura de parametro na URL

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -32,4 +32,11 @@ class WebAppApplicationTests {
 					.andExpect(content()
 							.string(containsString("Hello, Bruno!")));
 	}
+	
+	@Test
+	public void shouldReturnUrlParamMessage() throws Exception {
+		this.mockMvc.perform(get("/greeting/teste"))
+					.andDo(print()).andExpect(status().isOk())
+					.andExpect(content().string(containsString("Hello teste!")));
+	}
 }
