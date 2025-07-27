753a5e870bc375edde4bab53274f0b8265372ab5
Bruno Souza <souza.brunha@gmail.com>
2025-07-27 17:52:00 -0300

    feat: endpoint para leitura de query string na URL

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -2,6 +2,7 @@ package com.example.WebApp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,6 +16,11 @@ public class HomeController {
 	@RequestMapping("/bruno")
 	public @ResponseBody String greetingFulano() {
 		return "Hello, Bruno!";
+	}
+	
+	@RequestMapping("/greeting")
+	public @ResponseBody String greetingQueryParam(@RequestParam("param") String param) {
+		return "Hello " + param + "!";
 	}
 	
 	@RequestMapping("/greeting/{param}")
